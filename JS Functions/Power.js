3c60c087bc97057bdcdcb661b96becc3b764173f
Idris Paragulgov <58070651+ipa9@users.noreply.github.com>
2022-03-31 15:54:07 +0400

    Create Power.js

---
+++ JS Functions/Power.js
@@ -0,0 +1,16 @@
+const makeNoise = function() {
+  console.log("Pling!");
+};
+
+makeNoise();
+// → Pling!
+
+const power = function(base, exponent) {
+  let result = 1;
+  for (let count = 0; count < exponent; count++) {
+    result *= base;
+  }
+  return result;
+};
+
+console.log(power(2, 10));
